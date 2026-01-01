00c360917fffa2840a9061bdc1741c5853893602
kyire <paqi404notfound@gmail.com>
2026-01-01 10:12:24 +0800

    依赖修正

--- src/main/java/org/example/wsdemo2/config/WebSocketConfig.java
+++ src/main/java/org/example/wsdemo2/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package org.example.wsdemo2.config;
 
 import org.example.wsdemo2.handler.MyWebSocketHandler;
+import org.example.wsdemo2.interceptor.MyWsInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -10,11 +12,15 @@ import org.springframework.web.socket.server.support.HttpSessionHandshakeInterce
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+    @Autowired
+    private MyWebSocketHandler myWebSocketHandler;
+    @Autowired
+    private MyWsInterceptor myWsInterceptor;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MyWebSocketHandler(), "/ws") // WebSocket请求的路径
-                .addInterceptors(new HttpSessionHandshakeInterceptor()) // 可以加入会话拦截器
+        registry.addHandler(myWebSocketHandler, "/ws") // WebSocket请求的路径
+                .addInterceptors(myWsInterceptor) // 可以加入会话拦截器
                 .setAllowedOrigins("*"); // 允许跨域
     }
 }
